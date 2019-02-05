66f81b8244a14888c46ef360d6d1239ac18946b5
Carmen Renee <carmengenpt@gmail.com>
2019-02-05 18:27:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,14 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"}
-        
+      :patriarch => {
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        
+        name: "Lord Montague", age: "53"
         },
       :hero => {
-        
+        name: "Lord Montague", age: "53"
         },
       :hero_friends => []
    }, 
